056ee7ead80b0f5bc0160fafa6fc33053c18c828
Emansami1 <123333301+Emansami1@users.noreply.github.com>
2023-01-25 20:42:39 +0200

    Add files via upload

---
+++ Face_Recognition_Tool.py
@@ -0,0 +1,32 @@
+import cv2
+
+# Load the cascade classifier
+face_cascade = cv2.CascadeClassifier("path/to/haarcascade_frontalface_default.xml")
+
+# Open the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read a frame from the webcam
+    _, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+    # Draw a rectangle around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the frame
+    cv2.imshow("Webcam", frame)
+
+    # Exit the loop if the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close the window
+cap.release()
+cv2.destroyAllWindows()
